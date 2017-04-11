86fe89d6f9bcff66dfd90ad4779184d964544652
Levon Becker <levon.becker.github@bonusbits.com>
2017-04-10 21:11:23 -0700

    ## 1.0.1 - 04/10/2017 - Levon Becker * Fixed CloudFormation Chef Environment Cluster Attribute name. [ci skip]

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,7 @@
 # CHANGE LOG
 
+## 1.0.1 - 04/10/2017 - Levon Becker
+* Fixed CloudFormation Chef Environment Cluster Attribute name.
+
 ## 1.0.0 - 04/09/2017 - Levon Becker
 * Initial Release
--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'levon.becker.github@bonusbits.com'
 license 'MIT'
 description 'Deploy EC2 Autoscaling Instance, Install and Configure ECS Agent'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '1.0.0'
+version '1.0.1'
 issues_url 'https://github.com/bonusbits/bonusbits_ecs_cluster/issues'
 source_url 'https://github.com/bonusbits/bonusbits_ecs_cluster'
 
