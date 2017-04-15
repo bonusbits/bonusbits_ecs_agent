a18a45fee94ff7701df2f10d6b2723b58fdfb37c
Levon Becker <levon.becker.github@bonusbits.com>
2017-04-15 11:34:58 -0700

    1.0.3 - Read Changelog

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,10 @@
 # CHANGE LOG
 
+## 1.0.3 - 04/15/2017 - Levon Becker
+* Added CloudWatch Logs /var/log/chef-client.log stream
+* CFN: Added Client.rb chef client logging to client.rb **/var/log/chef-client.log**
+* CFN: Removed role since just calling the recipe for run list and change chef run to call the recipe instead.
+
 ## 1.0.2 - 04/13/2017 - Levon Becker
 * Upgraded and test with chefdk 1.3.40 / chef-client 12.19.36
 
--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'levon.becker.github@bonusbits.com'
 license 'MIT'
 description 'Deploy EC2 Autoscaling Instance, Install and Configure ECS Agent'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '1.0.2'
+version '1.0.3'
 chef_version '~> 12.5' if respond_to?(:chef_version)
 issues_url 'https://github.com/bonusbits/bonusbits_ecs_cluster/issues'
 source_url 'https://github.com/bonusbits/bonusbits_ecs_cluster'
